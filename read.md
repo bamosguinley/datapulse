6b6c992b76354a5f97d9ecb1ec46c834f5c26b06
Bamos Guinley <guinleyb@gmail.com>
2024-08-28 19:53:06 +0200

    add get

---
+++ read.md
@@ -0,0 +1,3 @@
+Un repository sert a marquer une classes comme étant un repository, c'est à dore un endroit où vous allez interagir avec votre bdd
+
+Il n'est pas obligatoire de mettre l'annotation 
---
+++ src/main/java/bj/highfiveuniversity/datapulse/controller/ProductController.java
@@ -0,0 +1,60 @@
+package bj.highfiveuniversity.datapulse.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import bj.highfiveuniversity.datapulse.model.Product;
+import bj.highfiveuniversity.datapulse.repository.ProductRepository;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+
+
+
+@RestController
+@RequestMapping("/api/products")
+public class ProductController {
+    @Autowired
+    private ProductRepository productRepository;
+    @GetMapping
+    public List<Product> getAllProducts() {
+        return productRepository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public Product getById(@PathVariable Long id) {
+        return productRepository.findById(id).orElseThrow();
+    }
+
+    @PostMapping
+    public Product createProduct(@RequestBody Product product) {
+        return productRepository.save(product);
+    }
+    
+    @PutMapping("/{id}")
+    public Product updateProduct(@PathVariable Long id , @RequestBody Product productDetail) {
+        Product product = productRepository.findById(id).orElseThrow();
+        product.setName(productDetail.getName());
+        product.setPrice(productDetail.getPrice());
+        return productRepository.save(product);
+    }
+    
+    @DeleteMapping("/{id}")
+    public String deleteProduct(@PathVariable Long id) {
+        Product product = productRepository.findById(id).orElseThrow();
+        productRepository.delete(product);
+        return "Delete succesfully";
+    }
+   
+    
+}
